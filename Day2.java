f358e9b880dfebf06b214a5fec910b4777e2ae74
SubhashreeP <subhashreep592@gmail.com>
2026-06-02 22:45:59 +0530

    Day-2 : Learning Variables

---
+++ Day2.java
@@ -0,0 +1,15 @@
+public class Day2 {
+
+    public static void main(String[] args){
+        int age = 21;
+        double cgpa = 8.9;
+        char grade = 'A';
+        String name = "Subhashree";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+
+    }
+}
