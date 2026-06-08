5cc22ac49e2b72d7f8ced8c65809db43b4658721
SubhashreeP <subhashreep592@gmail.com>
2026-06-08 23:54:04 +0530

    Day-7 : Learned for loop

---
+++ Day7.java
@@ -0,0 +1,11 @@
+// Write a Java program to print all even numbers from 1 to 30 using a for loop.
+
+public class Day7 {
+    public static void main(String[] args) {
+        for(int i = 2;i<=30;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
