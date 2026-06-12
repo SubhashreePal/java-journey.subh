083b4f8fbd8a257e6af5c058d44afcfbddd75b3c
SubhashreeP <subhashreep592@gmail.com>
2026-06-12 12:37:48 +0530

    Day-11 : String

---
+++ Day11.java
@@ -0,0 +1,17 @@
+public class Day11 {
+    public static void main(String[] args) {
+
+        String name = "Subhashree";
+
+        System.out.println(name);
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+        System.out.println(name.contains("as"));
+        System.out.println(name.contains("xyz"));
+
+
+    }
+}
