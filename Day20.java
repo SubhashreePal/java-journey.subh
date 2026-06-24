08602ac9f6bf0ccb6121ba1cab02f86c0b855ae2
SubhashreeP <subhashreep592@gmail.com>
2026-06-24 21:31:48 +0530

    Day-20 : Exception Handling

---
+++ Day20.java
@@ -0,0 +1,16 @@
+public class Day20 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30};
+
+        try {
+
+            System.out.println(arr[5]);
+
+        } catch (Exception e) {
+
+            System.out.println("Invalid Index");
+        }
+    }
+}
