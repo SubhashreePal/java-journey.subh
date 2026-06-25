6b73bff36bb59433e3c4f3cefc1d7732a88be73d
SubhashreeP <subhashreep592@gmail.com>
2026-06-25 12:11:18 +0530

    Day 21: Learned ArrayList

---
+++ Day21.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Day21 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Subhashree");
+        names.add("Java");
+        names.add("GitHub");
+
+
+        System.out.println(names);
+        System.out.println(names.get(0));
+        System.out.println(names.get(1));
+    }
+}
