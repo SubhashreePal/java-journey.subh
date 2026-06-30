ee1169a36717bfdd5ea7c247f6cf12874c2a2abb
SubhashreeP <subhashreep592@gmail.com>
2026-06-30 22:46:21 +0530

    Day-26 : Learned Generics

---
+++ Day26.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Day26 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers =
+                new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        System.out.println(numbers);
+    }
+}
