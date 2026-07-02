4cbdbb6a0789aa6ea1692cf1957e2e0483978724
SubhashreeP <subhashreep592@gmail.com>
2026-07-02 22:03:18 +0530

    Day-28 : LinkedList

---
+++ Day28.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class Day28 {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> tasks =
+                new LinkedList<>();
+
+        tasks.add("Study Java");
+        tasks.add("Practice DSA");
+        tasks.add("Push to GitHub");
+
+        System.out.println(tasks);
+    }
+}
