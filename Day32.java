b385ac8dc8473d34dc7a73fa7b3ff1d1ece13b8d
SubhashreeP <subhashreep592@gmail.com>
2026-07-06 21:46:20 +0530

    Day-32 : Learned Lambda Expressions

---
+++ Day32.java
@@ -0,0 +1,13 @@
+interface Add {
+    int sum(int a, int b);
+}
+
+public class Day32 {
+
+    public static void main(String[] args) {
+
+        Add add = (a, b) -> a + b;
+
+        System.out.println(add.sum(10, 20));
+    }
+}
