cfd9cdf1169c63c54f805c50b175a2551f0102a0
SubhashreeP <subhashreep592@gmail.com>
2026-07-08 21:20:11 +0530

    P-3 : Temperature Converter

---
+++ Main.java
@@ -0,0 +1,25 @@
+// Problem - 3 : Temperature Converter using Ternary Operator
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.print("Enter the Temperature : ");
+        temp = sc.nextDouble();
+
+        System.out.print("Convert to Celsius or Fahrenheit? (C or F) : ");
+        unit = sc.next().toUpperCase();
+
+        //ternary operator
+        // (condition) ? true : false
+        newTemp = (unit.equals("C")) ? (temp - 32) * 5 / 9 : (temp * 9 / 5) + 32;
+
+        System.out.println(newTemp + unit);
+
+        sc.close();
+    }
+}
