cc21c59e2e6ada5efe8e0633da78c6094bddcbb3
SubhashreeP <subhashreep592@gmail.com>
2026-07-13 22:02:51 +0530

    Day-37 : Stack Underflow

---
+++ Day37.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class Day37 {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> numbers = new Stack<>();
+
+        System.out.println(numbers.empty());
+
+        numbers.push(10);
+
+        System.out.println(numbers.empty());
+    }
+}
